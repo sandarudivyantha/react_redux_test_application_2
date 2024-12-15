1dc835d25ae3604d56ed682de23d23dc098fd99c
Sandaru Divyantha <79299187+sandarudivyantha@users.noreply.github.com>
2024-12-15 19:55:30 +0530

    INCREMENT AND DECREMENT FUNCTIONS CREATED

--- src/store/reducers/number.jsx
+++ src/store/reducers/number.jsx
@@ -7,6 +7,16 @@ const numberActionType = {
   DEC: "decrement",
 };
 
+export const numberIncrement = (payload = 1) => ({
+  type: numberActionType.INC,
+  payload,
+});
+
+export const numberDecrement = (payload = 1) => ({
+  type: numberActionType.DEC,
+  payload,
+});
+
 const reducer = (state = defaultState, { type, payload }) => {
   if (type === numberActionType.INC) {
     return { number: state.number + payload };
