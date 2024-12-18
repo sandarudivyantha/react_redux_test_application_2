0bf60caec25fe0a3b6b6fa213cccd62eadbffde6
Sandaru Divyantha <79299187+sandarudivyantha@users.noreply.github.com>
2024-12-18 17:16:53 +0530

    laptop handler creates

--- src/Components/laptopComponent.jsx
+++ src/Components/laptopComponent.jsx
@@ -4,8 +4,20 @@ import { laptopSliceSelector } from "../store/reducers/laptopSlice";
 const laptopComponent = () => {
   const laptop = useSelector(laptopSliceSelector);
 
+  const addLaptopHandler = () => {};
+
   return (
     <div>
+      <div>
+        <h3>Add Laptops</h3>
+        <input type="text" placeholder="Enter Price" />
+        <input type="text" placeholder="Enter CPU" />
+        <input type="text" placeholder="Enter Generation" />
+        <input type="text" placeholder="Enter RAM" />
+        <input type="text" placeholder="Enter HDD" />
+        <button onClick={addLaptopHandler}>Add Laptop</button>
+      </div>
+
       {laptop.map((ele) => (
         <p key={ele.id}>
           {ele.price}|{ele.spec.cpu}|{ele.spec.gen}|{ele.spec.ram}|
