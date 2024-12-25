46bb498d7ecc7281ea6b2269ae03590ac556d007
Sandaru Divyantha <79299187+sandarudivyantha@users.noreply.github.com>
2024-12-25 15:40:18 +0530

    add number reducer to store

--- src/Store/store.jsx
+++ src/Store/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postSliceReducer from "./Reducers/postSlice";
+import numberSliceReducer from "./Reducers/numberSlice";
 
 const store = configureStore({
   reducer: {
     post: postSliceReducer,
+    number: numberSliceReducer,
   },
 });
 
